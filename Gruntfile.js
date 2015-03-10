7ad5437e55c05f56e4e92b95ab74a54e0a7dd6d6
mohammed88 <mohammed.khan.cmr@gmail.com>
2015-03-10 23:00:37 +0100

    Using build tool (Grunt) to minify and lint JS

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: 'views/js/main.js',
+        dest: 'views/js/main.min.js'
+      },
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+         },
+      },
+      all: ['views/js/main.js']
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify']);
+};

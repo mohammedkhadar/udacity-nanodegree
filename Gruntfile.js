721662b35d62f6b041e9601413eca74f43808f22
mohammed88 <mohammed.khan.cmr@gmail.com>
2015-03-11 08:31:49 +0100

    Minifying and inlining CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,12 +21,41 @@ module.exports = function(grunt) {
       },
       all: ['views/js/main.js']
     },
+    concat: {
+      options: {
+        separator: ' ',
+      },
+      dist: {
+        src: ['views/css/bootstrap-grid.css', 'views/css/style.css'],
+        dest: 'views/css/final.css',
+      },
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'views/css',
+          src: ['final.css'],
+          dest: 'views/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    inline: {
+      dist: {
+        src: 'views/pizza-dev.html',
+        dest: 'views/pizza.html'
+      }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-inline');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'concat', 'cssmin', 'inline']);
 };

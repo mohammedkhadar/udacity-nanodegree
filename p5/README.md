a411e5b8d1533f7ffe0b19589546c97d4c90f60d
mohammed-impelsys <mohammed.khan.cmr@gmail.com>
2015-04-01 19:56:14 +0530

    Adding project feedback fixes

--- p5/README.md
+++ p5/README.md
@@ -1,12 +1,13 @@
 # Running the application
 
-1. Host the files under the p5 folder on a web server.
+1. Host the files under the root folder on a web server.
 2. Access index.html from a web broser to run the app.
+3. You can filter the most populares places by using the search bar.
 
 ## Notes:
 
-- This is the neighbourhood map project where we are exploring some of the most popular places in London (If you like to explore other places change the "LatLng" value).
-- We are using the knockout.js organizational library to maintain separation of concerns using the MVVM design pattern.
-- Model -> We are using the data list the of popular locations returned by the Foursquare API, to create an observable array of "new Locations()"
-- ModelView -> The ModelView has the location filtering functionality and binds the Model(Observables) and the View(Declarative Bindings)
-- View -> The HTML has declarative bindings which use the ViewModel properties as their data source.
+1. This is the neighbourhood map project where we are exploring some of the most popular places in London (If you like to explore other places change the "LatLng" value).
+2. We are using the knockout.js organizational library to maintain separation of concerns using the MVVM design pattern
+3.Model -> We are using the data list of popular locations returned by the Foursquare API, to create an observable array of "new Location()"
+4. ViewModel -> The ViewModel has the location filtering functionality and binds the Model(Observables) and the View(Declarative Bindings)
+5. View -> The HTML has declarative bindings which use the ViewModel properties as their data source.
--- p5/js/app.js
+++ p5/js/app.js
@@ -2,17 +2,21 @@
 	@author: Mohammed
 	
 	Notes:
-
-	This is the neighbourhood map project where we are exploring some of the most popular places in London (If you like to explore other places change the "LatLng" value).
-	We are using the knockout.js organizational library to maintain separation of concerns using the MVVM design pattern.
-	Model -> We are using the data list the of popular locations returned by the Foursquare API, to create an observable array of "new Locations()"
-	ModelView -> The ModelView has the location filtering functionality and binds the Model(Observables) and the View(Declarative Bindings)
-	View -> The HTML has declarative bindings which use the ViewModel properties as their data source.
+	1. This is the neighbourhood map project where we are exploring some of the most popular places in London (If you 
+	like to explore other places change the "LatLng" value).
+	2. We are using the knockout.js organizational library to maintain separation of concerns using the MVVM design 
+	pattern
+	3.Model -> We are using the data list of popular locations returned by the Foursquare API, to create an observable 
+	array of "new Location()"
+	4. ViewModel -> The ViewModel has the location filtering functionality and binds the Model(Observables) and the 
+	View(Declarative Bindings)
+	5. View -> The HTML has declarative bindings which use the ViewModel properties as their data source.
 
 */
 
-var LatLng = "51.5072,0.1275"; // London // 12.966,77.566 (Bangalore)
-var locationData = "https://api.foursquare.com/v2/venues/explore?ll=" + LatLng + "&limit=20&section=topPicks&day=any&time=any&locale=en&oauth_token=A5NTPJR3WYZ4I3N3KRB4411S0K4L5GBT2Y25K3QKHGWY3VP5&v=20150329";
+var latLng = "51.5072,0.1275"; // London // 12.966,77.566 (Bangalore)
+var locationData = "https://api.foursquare.com/v2/venues/explore?ll=" + latLng + "&limit=20&section=topPicks&day=any&" + 
+"time=any&locale=en&oauth_token=A5NTPJR3WYZ4I3N3KRB4411S0K4L5GBT2Y25K3QKHGWY3VP5&v=20150329";
 
 /* Model */
 
@@ -59,9 +63,19 @@ $.ajax({
 
 var ViewModel = function() {
 	var self = this;
+	/* Error handling: Check if the Google maps API has loaded */
+	try {
+		google.maps;
+	} catch(e) {
+		alert("Google maps API has failed to load please. Please contact your network administrator.");
+		return;
+	}
+	
 	/* google map related bindings */
 	self.locations = ko.observableArray(model);
-	self.map = new google.maps.Map(document.getElementById('map-canvas'), { center: self.locations()[0], zoom: 12, disableDefaultUI: true });
+	self.map = new google.maps.Map(document.getElementById('map-canvas'), { center: self.locations()[0], 
+																			zoom: 12, 
+																			disableDefaultUI: true });
 	self.infoWindow = new google.maps.InfoWindow({content: ""});
 	
 	/* ko observables for filtering the list view */
@@ -71,7 +85,11 @@ var ViewModel = function() {
 	/* ko observables & computed observables to toggle listview */
 	self.showListView = ko.observable(true);
 	self.toggleListView = function(){ self.showListView( ! self.showListView() ); };
-	self.toggleButtonClass = ko.computed( function(){ return this.showListView() ? "glyphicon-remove" : "glyphicon-th-list";}, this);
+	self.toggleButtonClass = ko.computed( function(){ 
+		return this.showListView() ? "glyphicon-remove" : "glyphicon-th-list";}, this);
+
+	/* ko obervable to track current location */
+	self.currentLocation = ko.observable(null);
 
 	/* function called to filter the list view entries */
 	self.filter = function() {
@@ -86,14 +104,16 @@ var ViewModel = function() {
 	};
 };
 
-/** custom binding for location. On creating each of the listview item, goole map markers are created for that location and added to the google maps
-	init() -> Initialize the google map markers, add it to the map and assigns "click" event handlers for list item and mapmarker
-	update() -> Called when the declarative bindings are updated; especially when "visible" observable is updated due to filtering operation. 
-				Here we are removing the map marker from the map
+/** Custom binding for location. On creating each of the listview item, goole map markers are created for that location 
+	and added to the google maps.
+	1. init() -> Initialize the google map markers, add it to the map and assigns "click" event handlers for list item 
+	and mapmarker.
+	2. update() -> Called when the declarative bindings are updated; especially when "visible" observable is updated due
+	 to filtering operation. Here we are removing the map marker from the map
 */
 ko.bindingHandlers.location = {
 
-	init: function(element, valueAccessor, allBindings){
+	init: function(element, valueAccessor, allBindings, viewModel, bindingContext){
 		var map = allBindings.get('map');
 		var location = allBindings.get('location');
 		var infoWindow = allBindings.get('infoWindow');
@@ -108,6 +128,7 @@ ko.bindingHandlers.location = {
 		/* Adding "click" event listeners to Google maps marker and list view item */
 		google.maps.event.addListener(googleMapsMarker, 'click', function(){
 			googleMapsMarker.setAnimation(google.maps.Animation.BOUNCE);
+			map.setCenter(location);
 			var timeout = setTimeout(function() { 
 				googleMapsMarker.setAnimation(null);
 				clearTimeout(timeout);
@@ -116,9 +137,20 @@ ko.bindingHandlers.location = {
 								location.category + "<br>" +
 								location.address.join("<br>") + "<br>" +
 								location.tel + "<br>" +
-								"<a href='" + "https://foursquare.com/v/" + location.id + "'target='_blank'><img class='fs-icon' src='images/foursquare-icon-16x16.png'>" + 
-								"<span>Rating: " + location.rating + "</span></a>");
+								"<a href='" + "https://foursquare.com/v/" + location.id + "'target='_blank'>" +
+									"<img class='fs-icon' src='images/foursquare-icon-16x16.png'>" + 
+									"<span>Rating: " + location.rating + "</span>" +
+								"</a>");
 			infoWindow.open(map, googleMapsMarker);
+			bindingContext.$parent.currentLocation(location); //element.scrollIntoView();
+
+			/* Hide list view for smaller screens; for screen width > 500, center marker if list view is open by panning
+			250 */
+			if(window.innerWidth < 500) {
+				bindingContext.$parent.showListView(false);
+			} else {
+				if(bindingContext.$parent.showListView()) map.panBy(-250, 0) ;
+			}
 		});
 
 		element.addEventListener("click", function(e){
@@ -131,5 +163,12 @@ ko.bindingHandlers.location = {
 		var map = allBindings.get('map');
 		var location = allBindings.get('location');
 		location.visible() ? location.googleMapsMarker.setMap(map) : location.googleMapsMarker.setMap(null);
+	}
+};
+
+/* Call the listview filter function in the ViewModel whrn the search input text is updated */
+ko.bindingHandlers.onTextUpdate = {
+	update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		bindingContext.$root.filter(allBindings.get('update'));
 	}
 };
